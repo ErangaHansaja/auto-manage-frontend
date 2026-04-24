938f07a1c608d2af75419c771092fc17afbcae02
shamina-dev <shamina-dev.shaminapalansooriya@gmail.com>
2026-04-24 07:48:26 +0530

    main structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./auth/login/login').then(m => m.Login)
+    },
+    {
+        path: 'admin-mechanic-panel',
+        loadComponent: () => import('./pages/admin-mechanic-panel/admin-mechanic-panel.component').then(m => m.AdminMechanicPanelComponent),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
+            },
+            {
+                path: 'customers',
+                loadComponent: () => import('./pages/customers/customers.component').then(m => m.CustomersComponent)
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+    }
+];
